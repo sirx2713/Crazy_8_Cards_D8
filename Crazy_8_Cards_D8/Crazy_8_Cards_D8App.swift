29cc4fcb60c2920d2830b5bfa93369b4b1b49e2f
Sirx <bradx2713@gmail.com>
2024-02-29 20:32:26 +0200

    Initial Commit

---
+++ Crazy_8_Cards_D8/Crazy_8_Cards_D8App.swift
@@ -0,0 +1,17 @@
+//
+//  Crazy_8_Cards_D8App.swift
+//  Crazy_8_Cards_D8
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/02/29.
+//
+
+import SwiftUI
+
+@main
+struct Crazy_8_Cards_D8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
